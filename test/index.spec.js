bee943ca1fc1b6cbbddfd1432c7bb9dcfc757d28
Ryan Pon <ryan@meetearnest.com>
2015-09-27 11:53:17 -0700

    Implement pool.apply

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,14 +2,12 @@
 
 var Pool   = require('../').Pool;
 var should = require('chai').should();
+var P      = require('bluebird');
+var _      = require('lodash');
 
 describe('Pool', function () {
 
   describe('#close', function () {
-
-  });
-
-  describe('#apply', function () {
 
   });
 
@@ -22,6 +20,32 @@ describe('Pool', function () {
         .then(function (res) {
           should.exist(res);
           res.should.eql([2, 4, 6, 8, 10]);
+        });
+    });
+
+    it('should perform multiple maps at once', function () {
+      var pool = new Pool(2);
+      var arr1 = _.range(0, 50);
+      var fn1 = function (n) {
+        return n * 5;
+      };
+      var arr2 = _.range(25, 750);
+      var fn2 = function (n) {
+        return n * 2;
+      };
+      var arr3 = _.range(1, 4);
+      var fn3 = function (n) {
+        return n;
+      };
+      return P.all([
+        pool.map(arr1, fn1),
+        pool.map(arr2, fn2),
+        pool.map(arr3, fn3)
+      ])
+        .spread(function (res1, res2, res3) {
+          res1.should.eql(arr1.map(fn1));
+          res2.should.eql(arr2.map(fn2));
+          res3.should.eql(arr3.map(fn3));
         });
     });
 
@@ -89,5 +113,38 @@ describe('Pool', function () {
     });
 
   });
+
+  describe('#apply', function () {
+
+    it('should be a convenience method for running map with a single argument', function () {
+      return new Pool(2).apply(5, function (n) {
+        return n * 10;
+      })
+        .then(function (res) {
+          should.exist(res);
+          res.should.equal(50);
+        });
+    });
+
+    it('should be able handle simultaneous calls', function () {
+      var fn = function (n) {
+        return n * 10;
+      };
+      var pool = new Pool(2);
+      return P.all([
+        pool.apply(1, fn),
+        pool.apply(2, fn),
+        pool.apply(3, fn),
+        pool.apply(4, fn),
+        pool.apply(5, fn),
+        pool.map([6], fn)
+      ])
+        .then(function (results) {
+          results.should.eql([10, 20, 30, 40, 50, [60]]);
+        });
+    });
+
+  });
+
 
 });
