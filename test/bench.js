203a731d83f2f3e1b7d0d225e6d6177b67702628
Ryan Pon <ryan@meetearnest.com>
2015-10-02 23:03:00 -0700

    Simplify bench, add default number of workers

--- test/bench.js
+++ test/bench.js
@@ -2,15 +2,13 @@
 
 var Pool = require('../').Pool;
 var _    = require('lodash');
-var P    = require('bluebird');
 
-// pool map has about 300ms overhead on my i7 mac with chunksize 1
-// pool map has about 100ms overhead on my i7 mac with default chunksize
+// Very basic bench for reference during changes
+//   - pool map has about 300ms overhead on my i7 mac with chunksize 1
+//   - pool map has about 100ms overhead on my i7 mac with default chunksize
 var base = _.range(10000);
-var fn = function () {
-  for (var i = 0; i < 100000; i++) {
-    i++;
-  }
+var fn = function (n) {
+  return n;
 };
 
 console.time('builtin map');
@@ -22,10 +20,7 @@ var pool = new Pool(6);
 console.timeEnd('createPool');
 
 console.time('pool map');
-P.all([
-  pool.map(base, fn, 3500),
-  pool.map(base, fn, 3500)
-])
+pool.map(base, fn)
   .then(function () {
     console.timeEnd('pool map');
     process.exit(0);
